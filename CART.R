ebc6cb3ee7ac542d80e553db1e3f47dd55ab2079
Alec Soudry <86265324+alecsoudry@users.noreply.github.com>
2022-02-09 22:36:33 -0500

    Add files via upload

---
+++ CART.R
@@ -0,0 +1,107 @@
+####### riding mower data #######
+mower.df <- read.csv("RidingMowers.csv")
+
+# plot riding mower data and show splits
+plot(Lot_Size ~ Income, data = mower.df, col = ifelse(mower.df$Ownership == "Owner", "black", "red"), 
+     pch = 16, xlab = "Income ($000s)", ylab = "Lot Size (000s of square feet)")
+legend("topright", c("owner", "non-owner"), pch = 16, col = c("black", "red"))
+abline(v = 60)
+segments(0, 21, 60, 21)
+segments(60, 20, 200, 20)
+segments(85, 0, 85, 20)
+segments(62, 0, 62, 20)
+
+####### Classification tree for riding mower ownership #######
+library(rpart)
+install.packages("rpart.plot")
+library(rpart.plot)
+
+# for the full tree...
+class.tree <- rpart(Ownership ~ .,          
+                    data = mower.df,        
+                    method = "class",       
+                    cp = 0,                 
+                    minsplit = 1)           
+
+## plot tree
+prp(class.tree,          
+    type = 1,           
+    extra = 1,           
+    varlen = -10,        
+    box.col = ifelse(class.tree$frame$var == "<leaf>", 'gray', 'white'))  
+
+
+####### classification trees for predicting acceptance of loan offer #######
+bank.df <- read.csv("UniversalBank.csv")
+bank.df <- bank.df[, -c(1, 5)] # drop ID and ZIP.Code
+bank.df$Education <- as.factor(bank.df$Education)
+
+# partition
+set.seed(1)
+train.index <- sample(rownames(bank.df), nrow(bank.df)*0.3)
+bank.train <- bank.df[train.index, ]
+valid.index <- setdiff(rownames(bank.df), train.index)
+bank.valid <- bank.df[valid.index, ]
+
+####### full classification tree #######
+bank.full.ct <- rpart(Personal.Loan ~ ., data = bank.train, method = "class", cp = 0, minsplit = 1)
+# count number of leaves
+length(bank.full.ct$frame$var[bank.full.ct$frame$var == "<leaf>"])
+# plot tree
+prp(bank.full.ct, type = 1, extra = 1, under = TRUE, varlen = -10,
+    box.col = ifelse(bank.full.ct$frame$var == "<leaf>", 'gray', 'white'))
+
+# classify records in the training data
+bank.full.ct.pred.train <- predict(bank.full.ct, bank.train, type = "class")
+# generate confusion matrix for training data
+library(caret)
+confusionMatrix(bank.full.ct.pred.train, 
+                as.factor(bank.train$Personal.Loan), 
+                positive = "1")
+
+# classify records in the validation data
+bank.full.ct.pred.valid <- predict(bank.full.ct, bank.valid, type = "class")
+confusionMatrix(bank.full.ct.pred.valid, 
+                as.factor(bank.valid$Personal.Loan), 
+                positive = "1")
+
+####### default classification tree #######
+bank.default.ct <- rpart(Personal.Loan ~ ., data = bank.train, method = "class")
+# count number of leaves
+length(bank.default.ct$frame$var[bank.default.ct$frame$var == "<leaf>"])
+# plot tree
+prp(bank.default.ct, type = 1, extra = 1, under = TRUE, varlen = -10,
+    box.col = ifelse(bank.default.ct$frame$var == "<leaf>", 'gray', 'white'))
+
+# classify records in the training data
+bank.default.ct.pred.train <- predict(bank.default.ct, bank.train, type = "class")
+# generate confusion matrix for training data
+confusionMatrix(bank.default.ct.pred.train, 
+                as.factor(bank.train$Personal.Loan), 
+                positive = "1")
+
+# classify records in the validation data
+bank.default.ct.pred.valid <- predict(bank.default.ct, bank.valid, type = "class")
+confusionMatrix(bank.default.ct.pred.valid, 
+                as.factor(bank.valid$Personal.Loan), 
+                positive = "1")
+
+####### finding the best pruned tree #######
+cv.ct <- rpart(Personal.Loan ~ ., 
+               data = bank.train, 
+               method = "class",
+               cp = 0, 
+               minsplit = 1, 
+               xval = 10)            
+printcp(cv.ct)
+
+# prune by using cp value that corresponds with best pruned tree
+pruned.ct <- prune(cv.ct, cp = 0.02256)
+prp(pruned.ct, type = 1, extra = 1, varlen = -10,
+    box.col = ifelse(pruned.ct$frame$var == "<leaf>", 'gray', 'white'))
+
+# classify records in the validation data
+pruned.ct.pred.valid <- predict(pruned.ct, bank.valid, type = "class")
+confusionMatrix(pruned.ct.pred.valid, 
+                as.factor(bank.valid$Personal.Loan), 
+                positive = "1")
